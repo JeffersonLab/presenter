4e2d5735bbf7fe95c87390fd13b23992fc22f19c
slominskir <ryan.slominski@gmail.com>
2023-03-22 15:45:34 -0400

    Remove unused imports

--- src/main/java/org/jlab/presenter/presentation/controller/Resize.java
+++ src/main/java/org/jlab/presenter/presentation/controller/Resize.java
@@ -1,12 +1,7 @@
 package org.jlab.presenter.presentation.controller;
 
-import org.jlab.presenter.business.session.PresentationFacade;
-
-import javax.ejb.EJB;
-import javax.ejb.EJBAccessException;
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,14 +10,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
