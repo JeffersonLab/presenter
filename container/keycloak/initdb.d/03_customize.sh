a4807d50b8467136bd189a379b9953c1a51bcd75
Ryan Slominski <Ryan.Slominski@gmail.com>
2026-06-17 10:26:19 -0400

    Bump JLab Keycloak to v2.6.0

---
+++ container/keycloak/initdb.d/03_customize.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Located in root of container
+. /kc-lib.sh
+
+echo "----------------"
+echo "| Create Roles |"
+echo "----------------"
+KC_ROLE_NAME=cc
+create_role
+
+echo "----------------"
+echo "| Create Users |"
+echo "----------------"
+KC_PASSWORD=password
+
+KC_USERNAME=user1
+KC_FIRSTNAME=James
+KC_LASTNAME=Johnson
+KC_EMAIL=user1@example.com
+create_user
+KC_ROLE_NAME=${KC_RESOURCE}-user
+assign_role
+
+KC_USERNAME=user2
+KC_FIRSTNAME=Robert
+KC_LASTNAME=Williams
+KC_EMAIL=user2@example.com
+create_user
+KC_ROLE_NAME=${KC_RESOURCE}-user
+assign_role
+
+KC_USERNAME=user3
+KC_FIRSTNAME=Michael
+KC_LASTNAME=Miller
+KC_EMAIL=user3@example.com
+create_user
+KC_ROLE_NAME=${KC_RESOURCE}-user
+assign_role
+KC_ROLE_NAME=cc
+assign_role
